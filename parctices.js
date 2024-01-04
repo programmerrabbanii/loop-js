7950ca87af43e0e450ead962eb8fd1c23ce85b07
programmerrabbanii <rabbanisarkar642@gmail.com>
2024-01-04 12:52:12 +0600

    adde the loop

---
+++ parctices.js
@@ -0,0 +1,33 @@
+// var montherGiveMoney=1000;
+// var apple=200;
+// var oranage=150;
+// var totalCost=apple+oranage;
+// var remaingAmount= montherGiveMoney -totalCost;
+// console.log(remaingAmount)
+
+// var myFatherGiveMoney=5000;
+// var book=200;
+// var car=555;
+// var lunchBill=188;
+// var hotelFre=660;
+// var educations=555;
+// var toatalCost=book+car+lunchBill+hotelFre+educations;
+// // console.log(toatalCost)
+// var remaingAmount=myFatherGiveMoney - toatalCost;
+// console.log(remaingAmount)
+
+// var bangla=75.25;
+// var mathematices=65.77;
+// var biology=80.55;
+// var chemestriy=50.55;
+// var physice=30.45;
+// var totalMarks=bangla+mathematices+biology+chemestriy+physice;
+// console.log(totalMarks)
+
+
+// var future=['i am going to be']
+// var will=['an awesome web developer']
+// var oneLine=future+will;
+// console.log(oneLine)
+
+var mySisterGiveMoney=119;
