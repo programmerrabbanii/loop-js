88e95da905351f1667494808ee50fca3972e3e1b
programmerrabbanii <rabbanisarkar642@gmail.com>
2024-01-08 12:54:11 +0600

    adde debagun

---
+++ even.js
@@ -0,0 +1,30 @@
+// var number=1;
+// while(number < 10){
+//     console.log('khuda lagse')
+//     number++
+    
+// }
+// for( var i=0; i <8; i++){
+//     console.log('hello word')
+//     console.log(i)
+// }
+
+
+
+// while( number < 9 ){
+//     console.log('dos ta hobi')
+//     number++;
+//     console.log(number)
+// }
+// for (var i=0; i< 9; i++){
+//     console.log('ami boro')
+//     console.log(i)
+// }
+
+var numbers=[12,33,55,66,44,65,765,43,34,45,100];
+
+for(var i = 0; i< numbers.length; i++){
+    var nuber = numbers[i]
+    console.log(nuber)
+
+}
